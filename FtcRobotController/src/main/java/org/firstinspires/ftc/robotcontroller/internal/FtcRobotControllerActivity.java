9b3d77c49ecc8f69f4b76221dcb8ea3774051163
LincolnFreedman <21freedmanl96@mybhs.org>
2018-10-16 16:30:34 -0700

    old code, good .xml, permissions could still be bad

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -50,12 +50,10 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -120,85 +118,13 @@ import org.firstinspires.ftc.robotcore.internal.ui.UILocation;
 import org.firstinspires.ftc.robotcore.internal.webserver.RobotControllerWebInfo;
 import org.firstinspires.ftc.robotcore.internal.webserver.WebServer;
 import org.firstinspires.inspection.RcInspectionActivity;
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import ftc.vision.FrameGrabber;
-
 @SuppressWarnings("WeakerAccess")
 public class FtcRobotControllerActivity extends Activity
-  {
-      // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
-      private CameraBridgeViewBase cameraBridgeViewBase;
-
-      void myOnCreate(){
-          getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-          cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.show_camera_activity_java_surface_view);
-          new FrameGrabber(cameraBridgeViewBase);
-      }
-
-      //when the "Grab" button is pressed
-      public void frameButtonOnClick(View v){
-      }
-
-      void myOnPause(){
-          if (cameraBridgeViewBase != null) {
-              cameraBridgeViewBase.disableView();
-          }
-      }
-
-      void myOnResume(){
-          if (!OpenCVLoader.initDebug()) {
-              Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-              OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-          } else {
-              Log.d(TAG, "OpenCV library found inside package. Using it!");
-              mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-          }
-      }
-
-      public void myOnDestroy() {
-          if (cameraBridgeViewBase != null) {
-              cameraBridgeViewBase.disableView();
-          }
-      }
-
-      private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-          @Override
-          public void onManagerConnected(int status) {
-              switch (status) {
-                  case LoaderCallbackInterface.SUCCESS:
-                      Log.i(TAG, "OpenCV Manager Connected");
-                      //from now onwards, you can use OpenCV API
-//          Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-                      cameraBridgeViewBase.enableView();
-                      break;
-                  case LoaderCallbackInterface.INIT_FAILED:
-                      Log.i(TAG, "Init Failed");
-                      break;
-                  case LoaderCallbackInterface.INSTALL_CANCELED:
-                      Log.i(TAG, "Install Cancelled");
-                      break;
-                  case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
-                      Log.i(TAG, "Incompatible Version");
-                      break;
-                  case LoaderCallbackInterface.MARKET_ERROR:
-                      Log.i(TAG, "Market Error");
-                      break;
-                  default:
-                      Log.i(TAG, "OpenCV Manager Install");
-                      super.onManagerConnected(status);
-                      break;
-              }
-          }
-      };
+{
   public static final String TAG = "RCActivity";
   public String getTag() { return TAG; }
 
@@ -320,7 +246,7 @@ public class FtcRobotControllerActivity extends Activity
         RobotLog.vv(TAG, "disabling Dragonboard and exiting robot controller");
         DragonboardLynxDragonboardIsPresentPin.getInstance().setState(false);
         AppUtil.getInstance().finishRootActivityAndExitApp();
-        }
+      }
       else {
         // Double-sure check that we can talk to the DB over the serial TTY
         DragonboardLynxDragonboardIsPresentPin.getInstance().setState(true);
@@ -575,8 +501,8 @@ public class FtcRobotControllerActivity extends Activity
       } else {
         Intent programmingModeIntent = new Intent(AppUtil.getDefContext(), ProgrammingModeActivity.class);
         programmingModeIntent.putExtra(
-            LaunchActivityConstantsList.PROGRAMMING_MODE_ACTIVITY_PROGRAMMING_WEB_HANDLERS,
-            new LocalByRefIntentExtraHolder(programmingWebHandlers));
+                LaunchActivityConstantsList.PROGRAMMING_MODE_ACTIVITY_PROGRAMMING_WEB_HANDLERS,
+                new LocalByRefIntentExtraHolder(programmingWebHandlers));
         startActivity(programmingModeIntent);
       }
       return true;
@@ -608,7 +534,7 @@ public class FtcRobotControllerActivity extends Activity
       startActivityForResult(intentConfigure, RequestCode.CONFIGURE_ROBOT_CONTROLLER.ordinal());
     }
     else if (id == R.id.action_settings) {
-	  // historical: this once erroneously used FTC_CONFIGURE_REQUEST_CODE_ROBOT_CONTROLLER
+      // historical: this once erroneously used FTC_CONFIGURE_REQUEST_CODE_ROBOT_CONTROLLER
       Intent settingsIntent = new Intent(AppUtil.getDefContext(), FtcRobotControllerSettingsActivity.class);
       startActivityForResult(settingsIntent, RequestCode.SETTINGS_ROBOT_CONTROLLER.ordinal());
       return true;
@@ -623,7 +549,7 @@ public class FtcRobotControllerActivity extends Activity
       return true;
     }
 
-   return super.onOptionsItemSelected(item);
+    return super.onOptionsItemSelected(item);
   }
 
   @Override
@@ -672,12 +598,12 @@ public class FtcRobotControllerActivity extends Activity
       callback.updateRobotStatus(controllerService.getRobotStatus());
       // Only show this first-time toast on headless systems: what we have now on non-headless suffices
       requestRobotSetup(LynxConstants.isRevControlHub()
-        ? new Runnable() {
-            @Override public void run() {
-              showRestartRobotCompleteToast(R.string.toastRobotSetupComplete);
-            }
-          }
-        : null);
+              ? new Runnable() {
+        @Override public void run() {
+          showRestartRobotCompleteToast(R.string.toastRobotSetupComplete);
+        }
+      }
+              : null);
     }
   }
 
@@ -721,8 +647,8 @@ public class FtcRobotControllerActivity extends Activity
     requestRobotSetup(new Runnable() {
       @Override public void run() {
         showRestartRobotCompleteToast(R.string.toastRestartRobotComplete);
-        }
-      });
+      }
+    });
   }
 
   private void showRestartRobotCompleteToast(@StringRes int resid) {
